d8f00eb35d35bbb90860ef2e910261f6a1b90fae
Patrick Becker <patrickgbecker@gmail.com>
2022-05-16 08:27:23 -0500

    Refactor

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,48 @@
+function createLibrary(library) {
+  return {
+      name: library,
+      shelves: {
+        fantasy: [],
+        fiction: [],
+        nonFiction: [],
+      }
+  }
+}
 
+function addBook(library, book) {
+  if (book.genre === 'fantasy') {
+    return library.shelves.fantasy.push(book);
+  } else if (book.genre === 'nonFiction') {
+    return library.shelves.nonFiction.push(book);
+  } else {
+    return library.shelves.fiction.push(book);
+  }
+}
+
+function checkoutBook(library, book, genre) {
+  var isOnShelf = library['shelves']['fantasy'][0] ||
+                  library['shelves']['nonFiction'][0] ||
+                  library['shelves']['fiction'][0];
+
+  if (!isOnShelf) {
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+  }
+  if (isOnShelf && genre === 'fantasy') {
+    library.shelves.fantasy.pop(book);
+    return `You have now checked out ${book} from the ${library.name}`
+  }
+  if (isOnShelf && genre === 'nonFiction') {
+    library.shelves.nonFiction.pop(book);
+    return `You have now checked out ${book} from the ${library.name}`
+  }
+  if (isOnShelf && genre === 'fiction') {
+    library.shelves.fiction.pop(book);
+    return `You have now checked out ${book} from the ${library.name}`
+  }
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
